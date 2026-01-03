6067d481ff8db44161f2d9e170abc302f12163af
Argietha <argiethaaa@gmail.com>
2026-01-03 12:27:28 +0700

    second push

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,16 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: "gd4xxp",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-});
+  reporter: 'mochawesome',
+  reporterOptions: {
+    overwrite: false,
+    html: false,
+    json: true,
+  },
+})
